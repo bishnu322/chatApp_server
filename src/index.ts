d117b112cea226abab59b5a53d61b7cbf7b2a597
bishnu_kohar <koharbishnu322@gmail.com>
2025-12-16 09:51:28 +0545

    sending message using socket.io

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,35 @@ import "dotenv/config";
 import { DB_CONNECTION } from "./config/db.config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 
-// all the routes import
+// routes
 import authRouter from "./routes/auth.route";
 import userRouter from "./routes/user.route";
 import chatRouter from "./routes/chat.route";
 import messageRouter from "./routes/message.route";
 
+// creating server using express and socket.io
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
 const PORT = process.env.PORT || 9000;
 const DB_URI = process.env.DB_URI ?? "";
 
-// invoking the database uri link from env file
-DB_CONNECTION(DB_URI as string);
+// DB connection
+DB_CONNECTION(DB_URI);
 
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ limit: "5mb", extended: true }));
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
@@ -27,16 +40,59 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("hi there");
+interface IOnlineUser {
+  socketId: string;
+  userId: string;
+}
+interface IOnlineUser {
+  userId: string;
+  socketId: string;
+}
+
+let onlineUser: IOnlineUser[] = [];
+
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+
+  socket.on("addNewUser", (userId: string) => {
+    // remove old socket if same user reconnects
+    onlineUser = onlineUser.filter((u) => u.userId !== userId);
+
+    onlineUser.push({
+      userId,
+      socketId: socket.id,
+    });
+
+    io.emit("getOnlineUser", onlineUser);
+    console.log("online users:", onlineUser);
+  });
+  // sending message
+
+  socket.on("sendMessage", ({ senderId, receiverId, message }) => {
+    const receiver = onlineUser.find((user) => user.userId === receiverId);
+
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", {
+        senderId,
+        message,
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    onlineUser = onlineUser.filter((user) => user.socketId !== socket.id);
+
+    io.emit("getOnlineUser", onlineUser);
+    console.log("user disconnected", socket.id);
+  });
 });
 
-//all the routes
+// routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/chat", chatRouter);
 app.use("/api/message", messageRouter);
 
-app.listen(PORT, () => {
-  console.log(`server is listening on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
